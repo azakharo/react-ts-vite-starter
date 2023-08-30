e6ecff02136355075fac313cf9eb683ab017a7d4
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2023-08-30 12:18:07 +0300

    changed the deps, provided vite.config and made changes for migration

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+﻿import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import * as path from "path";
+
+export default ({ mode }) => {
+  return defineConfig({
+    plugins: [react()],
+    define: {
+      "process.env.NODE_ENV": `"${mode}"`,
+    },
+    resolve: {
+      alias: {
+        'src': path.resolve(__dirname, './src'),
+        'IMAGES': path.resolve(__dirname, './src/assets/images')
+      },
+    },
+  })
+}
