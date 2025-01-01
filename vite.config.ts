e1b56cfbf1ad9029ddad1a86659f383e24f216d8
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2025-01-01 19:59:45 +0300

    rem vite eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import * as path from 'path';
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
-import eslint from 'vite-plugin-eslint';
 
 export default () => {
   return defineConfig({
@@ -12,7 +11,6 @@ export default () => {
       react(),
       // svgr options: https://react-svgr.com/docs/options/
       svgr({svgrOptions: {icon: true}}),
-      eslint({fix: true}),
     ],
     resolve: {
       alias: {
